f5a7fe95b465bb64cfd721808461721e02c32cfa
khaclinh <linhtk.dhbk@gmail.com>
2025-03-08 14:16:52 +0100

    validation response node

---
+++ research/Person_agent/constants.py
@@ -0,0 +1,13 @@
+
+# Response maximum attempts
+MAX_ATTEMPTS = 3
+
+# Response status
+RES_INFO_NOT_SUFFICIENT = "info_not_sufficient",
+RES_GOOD = "good"
+RES_NOT_GOOD = "not_good"
+
+# Tool types
+TOOL_EMAIL = "email"
+TOOL_MEDIA = "media"
+TOOL_MAIN_AGENT = "main_agent"
